abdfa6c11a68e9919553e1e7974acf78b19502db
Phailin Rybarczyk <rybarczykp@bzz.ch>
2022-05-24 12:24:30 +0200

    model classes and service

---
+++ src/main/java/ch/bzz/bookshelf/model/Publisher.java
@@ -0,0 +1,13 @@
+package ch.bzz.bookshelf.model;
+
+import ch.bzz.bookshelf.model.Book;
+
+import java.util.List;
+
+//publisher of one or more books
+
+public class Publisher {
+    private String publisherUUID;
+    private String publisher;
+    private List<Book> bookList;
+}
